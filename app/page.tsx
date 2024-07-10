cda8f8f71ced258242c044e17f4ab09fecbbb2a5
jcanoruiz <jcanoruiz@paypal.com>
2024-07-10 16:02:50 -0600

    card parallax demo

---
+++ app/page.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import Image from 'next/image';
+import styles from './main.styles.module.scss';
+import { projects } from './data/projects';
+import { Card } from './components/Card';
+import { useRef} from 'react';
+import { useScroll } from 'framer-motion';
+
+export default function Home() {
+
+  const containerRef = useRef(null)
+  const { scrollYProgress } = useScroll({ 
+    target: containerRef,
+    offset: ['start start', 'end end']
+  })
+
+  return (
+    <main ref={containerRef} className={styles.main}>
+      {projects.map((project, index) => {
+        const targetScale = 1 - ((projects.length - index) * 0.05)
+        return <Card key={`p_${index}`} {...project} index={index} range={[index * 0.25, 1]} targetScale={targetScale} progress={scrollYProgress} />;
+      })}
+    </main>
+  );
+}

b6987068605eaff222ef6f258d460af03e4ccab3
Juan Carlos Cano Ruiz <juancarloscanoruiz7@gmail.com>
2024-07-23 09:21:23 -0600

    add spring

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import styles from './main.styles.module.scss';
 import { projects } from './data/projects';
 import { Card } from './components/Card';
 import { useRef} from 'react';
-import { useScroll } from 'framer-motion';
+import { useScroll, useSpring } from 'framer-motion';
 
 export default function Home() {
 
@@ -15,11 +15,16 @@ export default function Home() {
     offset: ['start start', 'end end']
   })
 
+  const scrollYProgressSpring = useSpring(scrollYProgress, {
+    stiffness: 90,
+    damping: 20,
+  })
+
   return (
     <main ref={containerRef} className={styles.main}>
       {projects.map((project, index) => {
         const targetScale = 1 - ((projects.length - index) * 0.05)
-        return <Card key={`p_${index}`} {...project} index={index} range={[index * 0.25, 1]} targetScale={targetScale} progress={scrollYProgress} />;
+        return <Card key={`p_${index}`} {...project} index={index} range={[index * 0.25, 1]} targetScale={targetScale} progress={scrollYProgressSpring} />;
       })}
     </main>
   );
